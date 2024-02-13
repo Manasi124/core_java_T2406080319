eafe757d041ffaf0d01c232ad1903a277a4674a6
user <user@manasi>
2024-02-13 11:45:44 +0530

    final commit

---
+++ session_04/src/session_04/Date.java
@@ -0,0 +1,32 @@
+package session_04;
+
+public class Date {
+	private int dd,mm,yy;
+	//private final float PI;
+	
+	//public Date() {
+		//PI = 3.1423f;
+	// }
+	
+	public Date(int dd,int mm,int yy) {
+		this.dd=dd;
+		this.mm=mm;
+		this.yy=yy;
+	}
+
+	
+	public String toString() {
+		return "Date [dd=" + dd + ", mm=" + mm + ", yy=" + yy + "]";
+	}
+	//public static void main(String[] args) {
+		//final Date b = new Date (1,1,18);
+		//b new Date(1,2,2002);
+	}
+
+
+
+	
+	
+	
+
+
